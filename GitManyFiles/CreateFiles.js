989e20b0cf1ada2b0d294b621ff44028a6e65523
Ismail Bougnouch <ismail00b@gmail.com>
2014-10-13 09:36:23 +0200

    test

---
+++ GitManyFiles/CreateFiles.js
@@ -0,0 +1,40 @@
+﻿var iFolder = 2;
+var nFiles = 500;
+
+var currentFolder = ds.getModelFolder().path;
+
+var folder = Folder(currentFolder+ "testFolder"+iFolder);
+folder.create();
+
+for (var i = 0; i < nFiles; i++) {
+	if(i%2 == 0)
+	{
+		var f = File(folder.path+ "file "+i+".js");
+		f.create();
+	}
+	else if(i%3 == 0) 
+	{
+		var f = File(folder.path+ "file "+i+".css");
+		f.create();
+	}
+};
+
+var folder = Folder(currentFolder+ "testFolder"+(iFolder+1));
+folder.create();
+
+for (var i = 0; i < nFiles; i++) {
+	if(i%2 == 0)
+	{
+		var f = File(folder.path+ "file "+i+".js");
+		f.create();
+	}
+	else if(i%3 == 0) 
+	{
+		var f = File(folder.path+ "file "+i+".css");
+		f.create();
+	}
+};
+
+
+var outFolderFile = File(currentFolder+ "file"+iFolder+".js");
+outFolderFile.create();
